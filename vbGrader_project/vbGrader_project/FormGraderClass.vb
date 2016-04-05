d0d6c50a5ba47416f0131d0cb5c9aa4f68d934fb
Kody J Brand <brandk@uwplatt.edu>
2016-04-05 17:24:06 -0500

    Removed old project file, added GraderSolution

--- vbGrader_project/vbGrader_project/FormGraderClass.vb
+++
@@ -1,3 +0,0 @@
-﻿Public Class FormGraderClass
-
-End Class
--- vbGrader_project/vbGrader_project/vbGrader.vb
+++
@@ -1,5 +0,0 @@
-﻿Public Class vbGrader
-    Public Shared Sub Main()
-        Application.Run(New FormGraderClass)
-    End Sub
-End Class
